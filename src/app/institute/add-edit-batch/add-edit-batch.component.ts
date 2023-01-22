0f19b4e000f3c702da1c052205b64854f4b296d5
Amarjeetkumarchaurasia <bidupur8544@gmail.com>
2023-01-22 17:52:14 +0530

    batch

--- src/app/institute/add-edit-batch/add-edit-batch.component.ts
+++ src/app/institute/add-edit-batch/add-edit-batch.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-add-edit-batch',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit-batch.component.css']
 })
 export class AddEditBatchComponent implements OnInit {
+  disableSelect = new FormControl(false);
+  batch_form!: FormGroup;
+  admin = 1;
+  upload: any;
+  actionBtn: string = 'Submit'
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private matref: MatDialogRef<AddEditBatchComponent>,
+    @Inject(MAT_DIALOG_DATA) public edit_batch: any
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
+      return false;
+    };
   }
+  ngOnInit(): void {
+    this.batch_form = this.fb.group({
+      batch_id: [''],
+      batch_name: ['', Validators.required],
+      batch_select: ['', Validators.required],
+      start_batch: ['', Validators.required],
+      batch_date: ['', Validators.required],
+      batch_form: ['', Validators.required],
+      batch_to: ['', Validators.required],
+      admin_id_fk: ['', Validators.required]
+    })
+    this.batch_form.controls['batch_date'].setValue(new Date().toISOString().slice(0, 10));
 
+    /////////////////////////////////////////////////// For The Edit Course Data ////////////////////////////////////////////////////////////////
+
+    if (this.edit_batch) {
+      this.actionBtn = "Update";
+      this.batch_form.controls['batch_id'].setValue(this.edit_batch.batch_id);
+      this.batch_form.controls['batch_name'].setValue(this.edit_batch.batch_name);
+      this.batch_form.controls['batch_select'].setValue(this.edit_batch.batch_select);
+      this.batch_form.controls['start_batch'].setValue(this.edit_batch.start_batch);
+      this.batch_form.controls['batch_date'].setValue(this.edit_batch.batch_date);
+      this.batch_form.controls['batch_form'].setValue(this.edit_batch.batch_form);
+      this.batch_form.controls['batch_to'].setValue(this.edit_batch.batch_to);
+      this.batch_form.controls['admin_id_fk'].setValue(this.edit_batch.admin_id_fk);
+    }
+  }
+  batch_btn() {
+
+  }
 }
--- src/app/institute/admission/admission.component.ts
+++ src/app/institute/admission/admission.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
 
+export interface Userdata {
+  id: number;
+  std_id: number;
+  std_name: string;
+  std_mobile: number;
+  course: string;
+  batch: string;
+  date: string;
+  status: string;
+}
+
+const Userdata: Userdata[] = [
+  {id: 1, std_id: 2, std_name: 'Amarjeet Kumar', std_mobile: 9856232154, course:'MCA',batch:'Python',date:'20-02-2023',status:'Null'},
+  {id: 1, std_id: 2, std_name: 'Amarjeet Kumar', std_mobile: 9856232154, course:'MCA',batch:'Python',date:'20-02-2023',status:'Null'},
+  {id: 1, std_id: 2, std_name: 'Amarjeet Kumar', std_mobile: 9856232154, course:'MCA',batch:'Python',date:'20-02-2023',status:'Null'},
+  {id: 1, std_id: 2, std_name: 'Amarjeet Kumar', std_mobile: 9856232154, course:'MCA',batch:'Python',date:'20-02-2023',status:'Null'},
+  {id: 1, std_id: 2, std_name: 'Amarjeet Kumar', std_mobile: 9856232154, course:'MCA',batch:'Python',date:'20-02-2023',status:'Null'},
+  {id: 1, std_id: 2, std_name: 'Amarjeet Kumar', std_mobile: 9856232154, course:'MCA',batch:'Python',date:'20-02-2023',status:'Null'},
+  {id: 1, std_id: 2, std_name: 'Amarjeet Kumar', std_mobile: 9856232154, course:'MCA',batch:'Python',date:'20-02-2023',status:'Null'},
+  {id: 1, std_id: 2, std_name: 'Amarjeet Kumar', std_mobile: 9856232154, course:'MCA',batch:'Python',date:'20-02-2023',status:'Null'},
+  
+];
 @Component({
   selector: 'app-admission',
   templateUrl: './admission.component.html',
   styleUrls: ['./admission.component.css']
 })
+
 export class AdmissionComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'std_id', 'std_name', 'std_mobile','course','batch','date','status', 'action'];
+  dataSource = new MatTableDataSource(Userdata);
+  std_count: string="0"
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  tabledata: any;
+  paginatorRef: any;
 
-  constructor() { }
+  constructor(
+    private dailog: MatDialog,
+  ) {  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
-
 }
+
+
