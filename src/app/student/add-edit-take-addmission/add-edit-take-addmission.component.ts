c9cfc3b8ded2e94eee26092f1a39882f8a9f181f
munnamaical <munnamaical05@gmail.com>
2023-01-24 16:48:46 +0530

    admission design

--- src/app/student/add-edit-take-addmission/add-edit-take-addmission.component.ts
+++ src/app/student/add-edit-take-addmission/add-edit-take-addmission.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { AddEditAddmissionComponent } from 'src/app/institute/add-edit-addmission/add-edit-addmission.component';
 
 @Component({
   selector: 'app-add-edit-take-addmission',
   templateUrl: './add-edit-take-addmission.component.html',
   styleUrls: ['./add-edit-take-addmission.component.css']
 })
-export class AddEditTakeAddmissionComponent implements OnInit {
 
-  constructor() { }
+
+export class AddEditTakeAddmissionComponent implements OnInit {
+  addparty: any;
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
+  resetparty() {
+    throw new Error('Method not implemented.');
+  }
+  disableSelect = new FormControl(false);
+  selector: 'radio-overview-example' | undefined
+  InstForm!: FormGroup;
+  upload: any;
+  actionBtn: string = 'Submit'
+  admin_id: any;
+  constructor(
+    private fb: FormBuilder,
+    private matref: MatDialogRef<AddEditAddmissionComponent>,
+
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.InstForm = this.fb.group({
+      std_name: ['',Validators.required],
+      course_name: ['', ],
+      Father_name: ['', ],
+      Father_occuption: ['',],
+      Moblie: ['', Validators.required],
+      institute_country: ['',],
+      Aadhar: ['',],
+      Gender: ['', Validators.required],
+      Dob: ['', ],
+      Email: ['',],
+      Address: ['', Validators.required],
+      state: ['', ],
+      District: ['', ],
+      Date: ['', ],
+      course_date: ['',]
+    }
+    )}
+    addstd(){
 
+    }
 }
