0d9d1d846d7037a1816e9e3d1ee219d7444c574c
akhilesh <tech.kr.akhi@gmail.com>
2023-02-14 19:43:14 +0530

    std ques

--- src/app/student/quiz-dashboard/quiz-dashboard.component.ts
+++ src/app/student/quiz-dashboard/quiz-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ManageService } from 'src/app/manage.service';
 
 @Component({
@@ -13,12 +14,14 @@ export class QuizDashboardComponent implements OnInit {
   login:any
   inst_id:any
   course_quiz:any
+  course_data:any
   constructor(
+    private router:Router,
     private service: ManageService
   ) { 
     this.login_deatils = localStorage.getItem('Token')
     this.login = JSON.parse(this.login_deatils)
-    this.inst_id = this.login.inst_id
+    this.inst_id = this.login.institute_id_fk
   }
 
 
@@ -29,7 +32,12 @@ export class QuizDashboardComponent implements OnInit {
       (res: any) => {
         console.log(res.data)
         this.course_quiz  =  res.data
+      
       }
     )
+  }
+
+  onQuiz(event:any){
+    this.router.navigate(['/studenthome/quiz'] ,event )
   }
 }
--- src/app/student/quize/quize.component.ts
+++ src/app/student/quize/quize.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ManageService } from 'src/app/manage.service';
 @Component({
   selector: 'app-quize',
@@ -15,21 +16,31 @@ export class QuizeComponent implements OnInit {
   quiz_description: any
   ques_no: number = 1
   hidden: boolean = true;
+  course_data:any
+  inst_id:any
   constructor(
-    private service: ManageService
-  ) { }
+    private service: ManageService,
+    private router:Router
+  ) { 
+
+    const navigation = this.router.getCurrentNavigation();
+    this.course_data = navigation?.extras
+  }
   ngOnInit(): void {
-    this.service.get_quiz().subscribe(
+    const fromdata =  new FormData()
+    fromdata.append("course_id",this.course_data.course_id)
+    fromdata.append("inst_id", this.course_data.institute_id_fk)
+    this.service.get_quiz_by_inst_id(fromdata).subscribe(
       (res: any) => {
         console.log(res)
-        this.quizQuestion = res.data[0].quiz_question
-        this.quiz_option_a = res.data[0].quiz_option_a
-        this.quiz_option_b = res.data[0].quiz_option_b
-        this.quiz_option_c = res.data[0].quiz_option_c
-        this.quiz_option_d = res.data[0].quiz_option_d
-        this.quiz_description = res.data[0].quiz_description
-        this.count_admission = res.data.length
-        this.ques_no = res.data[0].quiz_id
+        // this.quizQuestion = res.data[0].quiz_question
+        // this.quiz_option_a = res.data[0].quiz_option_a
+        // this.quiz_option_b = res.data[0].quiz_option_b
+        // this.quiz_option_c = res.data[0].quiz_option_c
+        // this.quiz_option_d = res.data[0].quiz_option_d
+        // this.quiz_description = res.data[0].quiz_description
+        // this.count_admission = res.data.length
+        // this.ques_no = res.data[0].quiz_id
 
       }
     )
