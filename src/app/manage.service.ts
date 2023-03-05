4cb6451370e21644a976b54ec8770a044baa2c9a
Amarjeetkumarchaurasia <bidupur8544@gmail.com>
2023-03-05 11:40:54 +0530

    errr

--- src/app/manage.service.ts
+++ src/app/manage.service.ts
@@ -123,9 +123,6 @@ export class ManageService {
   student_conform(data: any) {
     return this.http.post<any>(this.baseUrl + 'student_conform.php', data);
   }
-  get_std_by_reg_no(data: any) {
-    return this.http.post<any>(this.baseUrl + 'get_std_by_reg_no.php', data);
-  }
 
 
   // for enquiry module 
