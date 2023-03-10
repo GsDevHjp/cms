791f85d01fc7e0c30a402bc2503591216b7f85e0
Amarjeetkumarchaurasia <bidupur8544@gmail.com>
2023-03-10 10:21:45 +0530

    institute

--- src/app/manage.service.ts
+++ src/app/manage.service.ts
@@ -9,7 +9,6 @@ export class ManageService {
     private http: HttpClient
   ) { }
   baseUrl: string = 'https://greensoft.net.in/gscms/api/';
-  // baseUrl: string = 'http://localhost/cmsapinew/';
  
   // dashboard  
   get_dashboad(data: any) {
